f07d5b7082043ecc9a61e50138297e357c276b0a
mimshins <mostafa.sh.coderino@gmail.com>
2022-07-20 13:35:05 +0430

    [:star:] add internals

---
+++ lib/internals/mixins.style.ts
@@ -0,0 +1,13 @@
+import { type CSSProperties } from "react";
+
+export const srOnlyCSSProperties = {
+  position: "absolute",
+  width: 1,
+  height: 1,
+  padding: 0,
+  margin: -1,
+  border: 0,
+  overflow: "hidden",
+  clip: "rect(0, 0, 0, 0)",
+  whiteSpace: "nowrap"
+} as CSSProperties;
