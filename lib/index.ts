386706b5fd2a241ea762ea3d995d75f7fcb6c78c
mimshins <mostafa.sh.coderino@gmail.com>
2022-07-24 13:47:59 +0430

    [⭐️] add Radio component

--- lib/index.ts
+++ lib/index.ts
@@ -1,4 +1,6 @@
 export * from "./Checkbox";
 export { default as Checkbox } from "./Checkbox";
+export * from "./Radio";
+export { default as Radio } from "./Radio";
 export * from "./Switch";
 export { default as Switch } from "./Switch";
