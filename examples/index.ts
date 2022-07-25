63ac548f9402632a5d16c4a6a92e20511d8aa308
mimshins <mostafa.sh.coderino@gmail.com>
2022-07-25 11:09:25 +0430

    [examples] add RadioGroup example

--- examples/index.ts
+++ examples/index.ts
@@ -1,4 +1,5 @@
 export { default as MyCheckbox } from "./Checkbox";
 export { default as MyCheckGroup } from "./CheckGroup";
 export { default as MyRadio } from "./Radio";
+export { default as MyRadioGroup } from "./RadioGroup";
 export { default as MySwitch } from "./Switch";
