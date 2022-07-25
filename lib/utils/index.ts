0b5843d9e9e5b605ba7ec30daa1bea584e2b23b9
mimshins <mostafa.sh.coderino@gmail.com>
2022-07-25 11:13:39 +0430

    [⭐️] add mathematics utilities

--- lib/utils/index.ts
+++ lib/utils/index.ts
@@ -1,4 +1,5 @@
 export { default as componentWithForwardedRef } from "./componentWithForwardedRef";
+export * from "./math";
 export { default as requestFormSubmit } from "./requestFormSubmit";
 export { default as useCheckBase } from "./useCheckBase";
 export { default as useEventCallback } from "./useEventCallback";
---
+++ lib/utils/math.ts
@@ -0,0 +1,26 @@
+export const clamp = (number: number, min: number, max: number): number =>
+  Math.max(Math.min(number, max), min);
+
+/**
+ * Linear interpolate on the scale given by `a` to `b`, using `t` as the point on that scale.
+ */
+export const lerp = (a: number, b: number, t: number) => a + t * (b - a);
+
+/**
+ * Inverse Linar Interpolation, get the fraction between `a` and `b` on which `v` resides.
+ */
+export const inLerp = (a: number, b: number, v: number) => (v - a) / (b - a);
+
+/**
+ * Remap values from one linear scale to another.
+ *
+ * `oMin` and `oMax` are the scale on which the original value resides,
+ * `rMin` and `rMax` are the scale to which it should be mapped.
+ */
+export const remap = (
+  v: number,
+  oMin: number,
+  oMax: number,
+  rMin: number,
+  rMax: number
+) => lerp(rMin, rMax, inLerp(oMin, oMax, v));
