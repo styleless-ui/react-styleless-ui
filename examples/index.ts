607a07ad6c689004299adfe0518f3de052c5ab92
mimshins <mostafa.sh.coderino@gmail.com>
2022-07-26 18:19:27 +0430

    [🔥] remove examples

--- examples/index.ts
+++
@@ -1,6 +0,0 @@
-export { default as MyBreadcrumb } from "./Breadcrumb";
-export { default as MyCheckbox } from "./Checkbox";
-export { default as MyCheckGroup } from "./CheckGroup";
-export { default as MyRadio } from "./Radio";
-export { default as MyRadioGroup } from "./RadioGroup";
-export { default as MySwitch } from "./Switch";
