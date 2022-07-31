b05326fd0ed33cdf63757729a54177eba067afd8
mimshins <mostafa.sh.coderino@gmail.com>
2022-07-31 18:02:33 +0430

    expose @utilityjs hooks

--- lib/utils/index.ts
+++ lib/utils/index.ts
@@ -1,3 +1,14 @@
+export { default as useControlledProp } from "@utilityjs/use-controlled-prop";
+export { default as useDeterministicId } from "@utilityjs/use-deterministic-id";
+export { default as useEventListener } from "@utilityjs/use-event-listener";
+export { default as useForkedRefs } from "@utilityjs/use-forked-refs";
+export { default as useIsMounted } from "@utilityjs/use-is-mounted";
+export { default as useIsomorphicLayoutEffect } from "@utilityjs/use-isomorphic-layout-effect";
+export { default as useOnChange } from "@utilityjs/use-on-change";
+export { default as useOnOutsideClick } from "@utilityjs/use-on-outside-click";
+export { default as usePreviousValue } from "@utilityjs/use-previous-value";
+export { default as useRegisterNodeRef } from "@utilityjs/use-register-node-ref";
+export { default as useScrollGuard } from "@utilityjs/use-scroll-guard";
 export { default as componentWithForwardedRef } from "./componentWithForwardedRef";
 export * from "./dom";
 export * from "./is";
