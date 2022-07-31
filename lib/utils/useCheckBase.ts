c28713288b3c66d98be7e89e0fc208c4d9d57803
mimshins <mostafa.sh.coderino@gmail.com>
2022-07-31 19:47:37 +0430

    [:sparkles:] improve keyboard interactions

--- lib/utils/useCheckBase.ts
+++ lib/utils/useCheckBase.ts
@@ -1,15 +1,16 @@
-import useControlledProp from "@utilityjs/use-controlled-prop";
-import useForkedRefs from "@utilityjs/use-forked-refs";
-import useIsMounted from "@utilityjs/use-is-mounted";
-import useIsomorphicLayoutEffect from "@utilityjs/use-isomorphic-layout-effect";
 import * as React from "react";
 import { type ICheckGroupContext } from "../CheckGroup/context";
+import { SystemKeys } from "../internals";
 import { type IRadioGroupContext } from "../RadioGroup/context";
 import {
   requestFormSubmit,
+  useControlledProp,
   useEventCallback,
-  useIsFocusVisible
-} from "../utils";
+  useForkedRefs,
+  useIsFocusVisible,
+  useIsMounted,
+  useIsomorphicLayoutEffect
+} from ".";
 
 interface CheckBaseProps {
   strategy?: "check-control" | "radio-control";
@@ -135,17 +136,14 @@ const useCheckBase = (props: CheckBaseProps) => {
     if (disabled || !isMounted()) return;
 
     if (isFocusedVisible) {
-      if (spaceKeyDownRef.current === false && event.key === " ")
+      if (spaceKeyDownRef.current === false && event.key === SystemKeys.SPACE)
         spaceKeyDownRef.current = true;
-      if (
-        enterKeyDownRef.current === false &&
-        event.key.toLowerCase() === "enter"
-      )
+      if (enterKeyDownRef.current === false && event.key === SystemKeys.ENTER)
         enterKeyDownRef.current = true;
     }
 
     if (event.target === event.currentTarget) {
-      if ([" ", "enter"].includes(event.key.toLowerCase())) {
+      if ([SystemKeys.SPACE, SystemKeys.ENTER].includes(event.key)) {
         event.preventDefault();
       }
 
@@ -159,29 +157,53 @@ const useCheckBase = (props: CheckBaseProps) => {
           ? window.getComputedStyle(currentRadio).direction
           : "ltr";
 
-        if (
-          ["arrowup", dir === "ltr" ? "arrowleft" : "arrowright"].includes(
-            event.key.toLowerCase()
-          )
-        ) {
+        const goPrev = [
+          SystemKeys.UP,
+          dir === "ltr" ? SystemKeys.LEFT : SystemKeys.RIGHT
+        ].includes(event.key);
+
+        const goNext = [
+          SystemKeys.DOWN,
+          dir === "ltr" ? SystemKeys.RIGHT : SystemKeys.LEFT
+        ].includes(event.key);
+
+        let activeRadio: typeof radios[number] | null = null;
+
+        const getAvailableRadio = (
+          idx: number,
+          forward: boolean,
+          prevIdxs: number[] = []
+        ): typeof activeRadio => {
+          const radio = radios[idx];
+
+          if (prevIdxs.includes(idx)) return null;
+
+          if (!radio || !radio[1].current || radio[1].current.disabled) {
+            const newIdx =
+              (forward ? idx + 1 : idx - 1 + radios.length) % radios.length;
+            return getAvailableRadio(newIdx, forward, [...prevIdxs, idx]);
+          }
+
+          return radio;
+        };
+
+        if (goPrev) {
+          activeRadio = getAvailableRadio(
+            (currentRadiosIdx - 1 + radios.length) % radios.length,
+            false
+          );
+        } else if (goNext) {
+          activeRadio = getAvailableRadio(
+            (currentRadiosIdx + 1) % radios.length,
+            true
+          );
+        }
+
+        if (activeRadio) {
           event.preventDefault();
 
-          const nextRadio =
-            radios[(currentRadiosIdx - 1 + radios.length) % radios.length];
-
-          nextRadio[1].current?.click();
-          nextRadio[1].current?.focus();
-        } else if (
-          ["arrowdown", dir === "ltr" ? "arrowright" : "arrowleft"].includes(
-            event.key.toLowerCase()
-          )
-        ) {
-          event.preventDefault();
-
-          const nextRadio = radios[(currentRadiosIdx + 1) % radios.length];
-
-          nextRadio[1].current?.click();
-          nextRadio[1].current?.focus();
+          activeRadio[1].current?.click();
+          activeRadio[1].current?.focus();
         }
       }
     }
@@ -194,16 +216,15 @@ const useCheckBase = (props: CheckBaseProps) => {
       if (disabled || !isMounted()) return;
 
       if (isFocusedVisible) {
-        if (event.key === " ") spaceKeyDownRef.current = false;
-        if (event.key.toLowerCase() === "enter")
-          enterKeyDownRef.current = false;
+        if (event.key === SystemKeys.SPACE) spaceKeyDownRef.current = false;
+        if (event.key === SystemKeys.ENTER) enterKeyDownRef.current = false;
       }
 
       onKeyUp?.(event);
 
       if (event.target === event.currentTarget) {
-        if (event.key === " ") emitChange(!checkedState);
-        else if (event.key.toLowerCase() === "enter")
+        if (event.key === SystemKeys.SPACE) emitChange(!checkedState);
+        else if (event.key === SystemKeys.ENTER)
           requestFormSubmit(event.target);
       }
     }
