ccf32a20536c997b75456f97a00d8ee6d3ae8c16
mimshins <mostafa.sh.coderino@gmail.com>
2022-12-20 18:27:48 +0330

    fix exports

--- lib/Snackbar/index.ts
+++ lib/Snackbar/index.ts
@@ -1,5 +1,5 @@
 export { default as Action, type ActionProps } from "./Action";
 export { default as Content, type ContentProps } from "./Content";
 export { default as Description, type DescriptionProps } from "./Description";
-export { default as Root, type SnackbarProps } from "./Snackbar";
+export { default as Root, type RootProps } from "./Snackbar";
 export { default as Title, type TitleProps } from "./Title";
