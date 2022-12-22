9b1e6598d93f2acc79970090a1b9eb5f5a9c59da
mimshins <mostafa.sh.coderino@gmail.com>
2022-12-23 01:12:18 +0330

    [:bug:] fix es6 minification

--- scripts/minify.ts
+++ scripts/minify.ts
@@ -8,8 +8,22 @@ const buildPath = path.join(packagePath, "./dist");
 
 const minifyFiles = async (files: string[]) => {
   for (const file of files) {
+    const isESModule = path.relative(buildPath, file).split("/")[0] === "esm";
+    const isIndex = path.basename(file) === "index.js";
+
+    if (isIndex) continue;
+
     const source = fs.readFileSync(file, { encoding: "utf8" });
-    const result = await minify(source);
+    const result = await minify(
+      source,
+      isESModule
+        ? {
+            module: isESModule,
+            compress: { module: isESModule },
+            mangle: { module: isESModule }
+          }
+        : undefined
+    );
 
     if (result.code) fs.writeFileSync(file, result.code);
   }
