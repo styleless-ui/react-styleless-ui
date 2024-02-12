c9ce4305e626a46ccec7ab1ad76340ec8f802f1e
mimshins <mostafa.sh.coderino@gmail.com>
2024-02-12 14:06:34 +0330

    Update tests

--- lib/TabGroup/TabGroup.test.tsx
+++ lib/TabGroup/TabGroup.test.tsx
@@ -22,21 +22,19 @@ describe("TabGroup", () => {
   itSupportsDataSetProps(TabGroup.Root, {});
 
   it("selects tabs with mouse interactions and calls `onChange` callback", async () => {
-    const handleChange = jest.fn<void, [tabIndex: number]>();
+    const handleChange = jest.fn<void, [tabValue: string]>();
 
     userEvent.setup();
     render(
       <TabGroup.Root onChange={handleChange}>
         <TabGroup.List label={tabListLabel}>
-          <TabGroup.Tab>Tab 1</TabGroup.Tab>
-          <TabGroup.Tab>Tab 2</TabGroup.Tab>
-          <TabGroup.Tab>Tab 3</TabGroup.Tab>
+          <TabGroup.Tab value="0">Tab 1</TabGroup.Tab>
+          <TabGroup.Tab value="1">Tab 2</TabGroup.Tab>
+          <TabGroup.Tab value="2">Tab 3</TabGroup.Tab>
         </TabGroup.List>
-        <TabGroup.Panels>
-          <TabGroup.Panel>Panel 1</TabGroup.Panel>
-          <TabGroup.Panel>Panel 2</TabGroup.Panel>
-          <TabGroup.Panel>Panel 3</TabGroup.Panel>
-        </TabGroup.Panels>
+        <TabGroup.Panel value="0">Panel 1</TabGroup.Panel>
+        <TabGroup.Panel value="1">Panel 2</TabGroup.Panel>
+        <TabGroup.Panel value="2">Panel 3</TabGroup.Panel>
       </TabGroup.Root>,
     );
 
@@ -48,34 +46,32 @@ describe("TabGroup", () => {
     await userEvent.click(tabs[0]!);
 
     expect(handleChange.mock.calls.length).toBe(1);
-    expect(handleChange.mock.calls[0]?.[0]).toBe(0);
+    expect(handleChange.mock.calls[0]?.[0]).toBe("0");
     expect(screen.getAllByRole("tabpanel").length).toBe(1);
     expect(screen.getByRole("tabpanel")).toHaveTextContent(/panel 1/i);
 
     await userEvent.click(tabs[2]!);
 
     expect(handleChange.mock.calls.length).toBe(2);
-    expect(handleChange.mock.calls[1]?.[0]).toBe(2);
+    expect(handleChange.mock.calls[1]?.[0]).toBe("2");
     expect(screen.getAllByRole("tabpanel").length).toBe(1);
     expect(screen.getByRole("tabpanel")).toHaveTextContent(/panel 3/i);
   });
 
   it("selects tabs with keyboard interactions and calls `onChange` callback", async () => {
-    const handleChange = jest.fn<void, [tabIndex: number]>();
+    const handleChange = jest.fn<void, [tabValue: string]>();
 
     userEvent.setup();
     render(
       <TabGroup.Root onChange={handleChange}>
         <TabGroup.List label={tabListLabel}>
-          <TabGroup.Tab>Tab 1</TabGroup.Tab>
-          <TabGroup.Tab>Tab 2</TabGroup.Tab>
-          <TabGroup.Tab>Tab 3</TabGroup.Tab>
+          <TabGroup.Tab value="0">Tab 1</TabGroup.Tab>
+          <TabGroup.Tab value="1">Tab 2</TabGroup.Tab>
+          <TabGroup.Tab value="2">Tab 3</TabGroup.Tab>
         </TabGroup.List>
-        <TabGroup.Panels>
-          <TabGroup.Panel>Panel 1</TabGroup.Panel>
-          <TabGroup.Panel>Panel 2</TabGroup.Panel>
-          <TabGroup.Panel>Panel 3</TabGroup.Panel>
-        </TabGroup.Panels>
+        <TabGroup.Panel value="0">Panel 1</TabGroup.Panel>
+        <TabGroup.Panel value="1">Panel 2</TabGroup.Panel>
+        <TabGroup.Panel value="2">Panel 3</TabGroup.Panel>
       </TabGroup.Root>,
     );
 
@@ -86,7 +82,7 @@ describe("TabGroup", () => {
     await userEvent.keyboard("[Space]");
 
     expect(handleChange.mock.calls.length).toBe(1);
-    expect(handleChange.mock.calls[0]?.[0]).toBe(0);
+    expect(handleChange.mock.calls[0]?.[0]).toBe("0");
     expect(screen.getAllByRole("tabpanel").length).toBe(1);
     expect(screen.getByRole("tabpanel")).toHaveTextContent(/panel 1/i);
 
@@ -95,31 +91,30 @@ describe("TabGroup", () => {
     await userEvent.keyboard("[Space]");
 
     expect(handleChange.mock.calls.length).toBe(2);
-    expect(handleChange.mock.calls[1]?.[0]).toBe(2);
+    expect(handleChange.mock.calls[1]?.[0]).toBe("2");
     expect(screen.getAllByRole("tabpanel").length).toBe(1);
     expect(screen.getByRole("tabpanel")).toHaveTextContent(/panel 3/i);
   });
 });
 
-describe("TabList", () => {
+describe("TabGroup.List", () => {
   afterEach(jest.clearAllMocks);
 
-  const REQUIRED_PROPS: TabGroup.ListProps = {
-    label: tabListLabel,
-    classes: { label: "label", root: "root" },
-  };
+  const Base = () => (
+    <TabGroup.Root>
+      <TabGroup.List label={"label"}></TabGroup.List>
+    </TabGroup.Root>
+  );
 
-  itShouldMount(TabGroup.List, REQUIRED_PROPS);
-  itSupportsRef(TabGroup.List, REQUIRED_PROPS, HTMLDivElement);
-  itSupportsStyle(TabGroup.List, REQUIRED_PROPS, "[role='tablist']");
-  itSupportsDataSetProps(TabGroup.List, REQUIRED_PROPS, "[role='tablist']");
+  itShouldMount(Base, {});
 
   it("should have `aria-label='label'` property when `label={{ screenReaderLabel: 'label' }}`", () => {
     render(
-      <TabGroup.List
-        {...REQUIRED_PROPS}
-        label={{ screenReaderLabel: tabListLabel }}
-      />,
+      <TabGroup.Root>
+        <TabGroup.List
+          label={{ screenReaderLabel: tabListLabel }}
+        ></TabGroup.List>
+      </TabGroup.Root>,
     );
 
     expect(screen.getByRole("tablist")).toHaveAttribute(
@@ -130,10 +125,9 @@ describe("TabList", () => {
 
   it("should have `aria-labelledby='identifier'` property when `label={{ labelledBy: 'identifier' }}`", () => {
     render(
-      <TabGroup.List
-        {...REQUIRED_PROPS}
-        label={{ labelledBy: "identifier" }}
-      />,
+      <TabGroup.Root>
+        <TabGroup.List label={{ labelledBy: "identifier" }}></TabGroup.List>
+      </TabGroup.Root>,
     );
 
     expect(screen.getByRole("tablist")).toHaveAttribute(
