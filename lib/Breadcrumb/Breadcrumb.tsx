e079667ff2053f5dfe8b98b7b5db336e4ae91473
mimshins <mostafa.sh.coderino@gmail.com>
2024-03-17 13:37:42 +0330

    [♻️][Breadcrumb]: Refactor to cleanup and improve readability

--- lib/Breadcrumb/Breadcrumb.tsx
+++ lib/Breadcrumb/Breadcrumb.tsx
@@ -1,17 +1,13 @@
 import * as React from "react";
 import { getLabelInfo, logger } from "../internals";
-import type { Classes, MergeElementProps } from "../types";
+import type { MergeElementProps } from "../types";
 import {
   componentWithForwardedRef,
   isFragment,
   useDeterministicId,
 } from "../utils";
-import { Item, SeparatorItem, type ItemProps } from "./components";
-import {
-  Label as LabelSlot,
-  List as ListSlot,
-  Root as RootSlot,
-} from "./slots";
+import { List } from "./components";
+import { Root as RootSlot } from "./slots";
 
 type OwnProps = {
   /**
@@ -19,14 +15,13 @@ type OwnProps = {
    */
   children?: React.ReactNode;
   /**
-   * Map of sub-components and their correlated classNames.
+   * The className applied to the component.
    */
-  classes?: Classes<"root" | "label" | "list">;
+  className?: string;
   /**
    * The label of the breadcrumb.
    */
   label:
-    | string
     | {
         /**
          * The label to use as `aria-label` property.
@@ -45,7 +40,7 @@ type OwnProps = {
 
 export type Props = Omit<
   MergeElementProps<"nav", OwnProps>,
-  "className" | "defaultChecked" | "defaultValue"
+  "defaultChecked" | "defaultValue"
 >;
 
 const BreadcrumbBase = (props: Props, ref: React.Ref<HTMLElement>) => {
@@ -53,39 +48,19 @@ const BreadcrumbBase = (props: Props, ref: React.Ref<HTMLElement>) => {
     label,
     children: childrenProp,
     id: idProp,
-    classes,
+    className,
     ...otherProps
   } = props;
 
   const id = useDeterministicId(idProp, "styleless-ui__breadcrumb");
-  const visibleLabelId = id ? `${id}__label` : undefined;
 
-  const listRefCallback = (node: HTMLOListElement | null) => {
-    if (!node) return;
-
-    const listItems = Array.from(node.children);
-    const lastItem = listItems[listItems.length - 1];
-
-    if (!lastItem?.firstElementChild) return;
-
-    if (lastItem.firstElementChild.tagName === "A") {
-      const anchorLink = lastItem.firstElementChild as HTMLAnchorElement;
-
-      if (anchorLink.hasAttribute("aria-current")) return;
-
-      logger(
-        [
-          "The aria attribute `aria-current`" +
-            " is missing from the last <BreadcrumbItem>'s anchor element.",
-          "For more information check out: " +
-            "https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Attributes/aria-current",
-        ].join("\n"),
-        { scope: "Breadcrumb", type: "warn" },
-      );
-    }
-  };
-
-  const labelProps = getLabelInfo(label, "Breadcrumb");
+  const labelInfo = getLabelInfo(label, "Breadcrumb", {
+    customErrorMessage: [
+      "Invalid `label` property.",
+      "The `label` property must be in shape of " +
+        "`{ screenReaderLabel: string; } | { labelledBy: string; }`",
+    ].join("\n"),
+  });
 
   const children = React.Children.map(childrenProp, child => {
     if (!React.isValidElement(child) || isFragment(child)) {
@@ -97,60 +72,30 @@ const BreadcrumbBase = (props: Props, ref: React.Ref<HTMLElement>) => {
       return null;
     }
 
-    if (
-      (child as React.ReactElement).type !== Item &&
-      (child as React.ReactElement).type !== SeparatorItem
-    ) {
+    if ((child as React.ReactElement).type !== List) {
       logger(
-        "The <Breadcrumb.Root> component only accepts <Breadcrumb.Item> and " +
-          "<Breadcrumb.Separator> as a children",
+        "The <Breadcrumb.Root> component only accepts <Breadcrumb.List> as a children.",
         { scope: "Breadcrumb", type: "error" },
       );
 
       return null;
     }
 
-    return child as React.ReactElement<ItemProps>;
+    return child as React.ReactElement;
   });
 
-  const renderLabel = () => {
-    if (!labelProps.visibleLabel) return null;
-
-    return (
-      <span
-        id={visibleLabelId}
-        data-slot={LabelSlot}
-        className={classes?.label}
-      >
-        {labelProps.visibleLabel}
-      </span>
-    );
-  };
-
   return (
-    <>
-      {renderLabel()}
-      <nav
-        {...otherProps}
-        id={id}
-        ref={ref}
-        className={classes?.root}
-        data-slot={RootSlot}
-        aria-label={labelProps.srOnlyLabel}
-        aria-labelledby={
-          labelProps.visibleLabel ? visibleLabelId : labelProps.labelledBy
-        }
-      >
-        <ol
-          ref={listRefCallback}
-          tabIndex={-1}
-          className={classes?.list}
-          data-slot={ListSlot}
-        >
-          {children}
-        </ol>
-      </nav>
-    </>
+    <nav
+      {...otherProps}
+      id={id}
+      ref={ref}
+      className={className}
+      data-slot={RootSlot}
+      aria-label={labelInfo.srOnlyLabel}
+      aria-labelledby={labelInfo.labelledBy}
+    >
+      {children}
+    </nav>
   );
 };
 
--- lib/Breadcrumb/components/index.ts
+++ lib/Breadcrumb/components/index.ts
@@ -1,4 +1,5 @@
 export { default as Item, type Props as ItemProps } from "./Item";
+export { default as List, type Props as ListProps } from "./List";
 export {
   default as SeparatorItem,
   type Props as SeparatorItemProps,
