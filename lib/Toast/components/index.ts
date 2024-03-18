014d5d0c636251b5489be41963d312af3d62b1c6
mimshins <mostafa.sh.coderino@gmail.com>
2024-03-18 13:34:40 +0330

    [🔥]: Remove unnecessary exports

--- lib/Toast/components/index.ts
+++ lib/Toast/components/index.ts
@@ -1,7 +1,2 @@
-export {
-  default as Action,
-  type ClassNameProps as ActionClassNameProps,
-  type Props as ActionProps,
-  type RenderProps as ActionRenderProps,
-} from "./Action";
+export { default as Action, type Props as ActionProps } from "./Action";
 export { default as Content, type Props as ContentProps } from "./Content";
