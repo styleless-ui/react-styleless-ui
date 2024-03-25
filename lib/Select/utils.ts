69ea876ff7530f62058d63db79c79385b0b35ba3
mimshins <mostafa.sh.coderino@gmail.com>
2024-03-25 16:51:42 +0330

    [♻️][Utils]: Move `useElementsRegistry` to Select utils

--- lib/Select/utils.ts
+++ lib/Select/utils.ts
@@ -59,3 +59,49 @@ export const getOptions = (
     return [...result, ...options];
   }, [] as Array<PickAsMandatory<OptionProps, "disabled" | "value" | "valueLabel">>);
 };
+
+type Registry<Key extends string> = Map<Key, string>;
+
+export type ElementsRegistry<Key extends string> = {
+  registerElement: (key: Key, id: string) => void;
+  unregisterElement: (key: Key) => void;
+  getElementId: (key: Key) => string | undefined;
+  getRegistry: () => Registry<Key>;
+};
+
+export const useElementsRegistry = <
+  Key extends string = string,
+>(): ElementsRegistry<Key> => {
+  const registryRef = React.useRef(new Map() as Registry<Key>);
+
+  return React.useMemo(() => {
+    type T = ElementsRegistry<Key>;
+
+    const getRegistry = () => registryRef.current;
+
+    const registerElement: T["registerElement"] = (key, id) => {
+      const registry = getRegistry();
+
+      registry.set(key, id);
+    };
+
+    const unregisterElement: T["unregisterElement"] = (key: Key) => {
+      const registry = getRegistry();
+
+      registry.delete(key);
+    };
+
+    const getElementId: T["getElementId"] = (key: Key) => {
+      const registry = getRegistry();
+
+      return registry.get(key);
+    };
+
+    return {
+      registerElement,
+      unregisterElement,
+      getElementId,
+      getRegistry,
+    };
+  }, []);
+};
