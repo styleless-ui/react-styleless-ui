6b75a829d53aec5d11e2d6f18b1ea991aba48690
mimshins <mostafa.sh.coderino@gmail.com>
2024-03-25 21:15:20 +0330

    Add `.npmrc` and `.npmignore` to package

--- scripts/build-package.ts
+++ scripts/build-package.ts
@@ -126,9 +126,26 @@ const createMainPackage = async () => {
   );
 };
 
+const createNPMRC = async () => {
+  const npmrcPath = path.join(distPath, ".npmrc");
+  const npmignorePath = path.join(distPath, ".npmignore");
+
+  await fs.writeFile(
+    npmrcPath,
+    [
+      "//registry.npmjs.org/:_authToken=${NODE_AUTH_TOKEN}",
+      "registry=https://registry.npmjs.org/",
+      "always-auth=true",
+    ].join("\n"),
+  );
+
+  await fs.writeFile(npmignorePath, ".npmrc");
+};
+
 void (async () => {
   await createModulePackages();
   await createMainPackage();
+  await createNPMRC();
 
   await fs.copyFile(readme, path.join(distPath, README_FILE_NAME));
   await fs.copyFile(license, path.join(distPath, LICENSE_FILE_NAME));
