3326afb5ff4dce9c1e98a9e45d9feb06b28b2dc6
mimshins <mostafa.sh.coderino@gmail.com>
2024-03-25 17:11:28 +0330

    [♻️]: Format the file

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,14 +11,39 @@ class _Document extends Document {
     return (
       <Html lang="en-US">
         <Head>
-          <meta httpEquiv="x-ua-compatible" content="ie=edge" />
-          <meta property="robots" content="Index, Follow" />
-          <meta name="mobile-web-app-capable" content="yes" />
-          <meta name="apple-mobile-web-app-capable" content="yes" />
-          <meta name="google" content="notranslate" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link rel="preload" as="style" href={googleFontFamily} />
-          <link rel="stylesheet" href={googleFontFamily} />
+          <meta
+            httpEquiv="x-ua-compatible"
+            content="ie=edge"
+          />
+          <meta
+            property="robots"
+            content="Index, Follow"
+          />
+          <meta
+            name="mobile-web-app-capable"
+            content="yes"
+          />
+          <meta
+            name="apple-mobile-web-app-capable"
+            content="yes"
+          />
+          <meta
+            name="google"
+            content="notranslate"
+          />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+          />
+          <link
+            rel="preload"
+            as="style"
+            href={googleFontFamily}
+          />
+          <link
+            rel="stylesheet"
+            href={googleFontFamily}
+          />
         </Head>
         <body>
           <Main />
