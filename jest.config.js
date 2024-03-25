e571d94969e2577abe2e05725bfdbb8ccdd0f1c8
mimshins <mostafa.sh.coderino@gmail.com>
2024-03-25 19:11:15 +0330

    [🔧][Configs]: Update configs

--- jest.config.js
+++ jest.config.js
@@ -2,12 +2,16 @@ import nextJest from "next/jest.js";
 
 const createJestConfig = nextJest({ dir: "./" });
 
+/**
+ * @type {import("jest").Config}
+ */
 const jestConfig = {
   verbose: true,
   setupFilesAfterEnv: ["<rootDir>/tests/jest.setup.ts"],
   testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"],
   testRegex: ".*\\.test\\.tsx?$",
   testEnvironment: "jest-environment-jsdom",
+  preset: "ts-jest",
 };
 
 export default createJestConfig(jestConfig);
